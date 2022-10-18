1ad6c98e545e0d18bd3cd614e4831b37cb8923e6
Nick <nsantander1095@gmail.com>
2022-10-18 09:15:11 -0400

    added asset caching (maybe), fixed a bug with dist being wrong for sw, added manifest logic for icons

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,6 +30,13 @@ module.exports = () => {
         fingerprints: false,
         start_url: '/',
         publicPath: '.',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
